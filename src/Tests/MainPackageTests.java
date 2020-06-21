c6879430734c4109f94755d2a047a11e6cf4a076
TrulsMoholt <truls.moholt@computas.com>
2020-06-21 13:46:21 +0200

    lagde ferdig test

--- src/Tests/MainPackageTests.java
+++ src/Tests/MainPackageTests.java
@@ -1,10 +1,12 @@
 package Tests;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 public class MainPackageTests{
     @Test
     public static void CanContainTest(){
-
+        Assertions.assertEquals(1,2);
     }
 
 
